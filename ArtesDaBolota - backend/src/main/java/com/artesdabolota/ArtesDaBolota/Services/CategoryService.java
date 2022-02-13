d149b7e5b52ea28fad926b93b548ed070965ca21
Gabriel <prismaticnetwork12@gmail.com>
2022-02-13 15:11:02 -0300

    feat: adição para pesquisar por único id

--- ArtesDaBolota - backend/src/main/java/com/artesdabolota/ArtesDaBolota/Services/CategoryService.java
+++ ArtesDaBolota - backend/src/main/java/com/artesdabolota/ArtesDaBolota/Services/CategoryService.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -24,5 +26,11 @@ public class CategoryService {
             listDto.add(new CategoryDTO(category));
         }
         return listDto;
+    }
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category category = obj.get();
+        return new CategoryDTO(category);
     }
 }
