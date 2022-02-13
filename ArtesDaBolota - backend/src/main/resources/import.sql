d564184c1b4ad32ce9096239bf1841833c293d80
Gabriel <prismaticnetwork12@gmail.com>
2022-02-13 14:01:54 -0300

    feat: database seeding

---
+++ ArtesDaBolota - backend/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_category(name) VALUES ('Books');
+INSERT INTO tb_category(name) VALUES ('Eletronics');
+INSERT INTO tb_category(name) VALUES ('Art');
+INSERT INTO tb_category(name) VALUES ('Games');
+INSERT INTO tb_category(name) VALUES ('Esports');
+INSERT INTO tb_category(name) VALUES ('Movies');
+INSERT INTO tb_category(name) VALUES ('Music');
