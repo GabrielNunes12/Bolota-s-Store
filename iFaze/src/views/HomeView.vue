0ec6ac971ab338c4243e3696e5b5895f5560a7d2
Gabriel <prismaticnetwork12@gmail.com>
2022-03-29 17:58:20 -0300

    feat: Project created

---
+++ iFaze/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script>
+import TheWelcome from "@/components/TheWelcome.vue";
+export default {
+  components: {
+    TheWelcome,
+  },
+  data() {
+    return {
+      message: "Welcome TO VUEJS3",
+    };
+  },
+};
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
